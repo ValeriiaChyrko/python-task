e31fee646cc72a91e12a892db90f4134e0cd8144
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-11-17 14:10:15 +0200

    Change structure.

---
+++ tests/test_my_module.py
@@ -0,0 +1,21 @@
+import unittest
+from src.my_module import greet, is_even
+
+class TestMyModule(unittest.TestCase):
+
+    def test_greet_passed(self):
+        self.assertEqual(greet("Іван"), "Привіт, Іван!")
+        self.assertEqual(greet("Марія"), "Привіт, Марія!")
+
+    def test_is_even_passed(self):
+        self.assertTrue(is_even(2))
+        self.assertTrue(is_even(0))
+        self.assertFalse(is_even(1))
+        self.assertFalse(is_even(-1))
+        self.assertTrue(is_even(-2))
+
+    def test_greet_failed(self):
+        self.assertEqual(greet("Марія"), "Привіт, Іван!")
+
+if __name__ == '__main__':
+    unittest.main()
